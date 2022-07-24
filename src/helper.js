93a0173c7d435779203e7c6988f3877252268ca3
DanielK <Danielkang13@gmail.com>
2022-07-24 14:05:15 -0700

    added helper functions to return html string with the employee info

--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,7 @@
 // create functions that are going to be used multiple times
 // place functions here to remove cluster from other js files?
 
-function generateHtml() { // returns a basic layout of the webpage, we can then append new 'cards' dynamically
+function generateHtml(managerStr, engineerStr, internStr) { // returns a basic layout of the webpage, we can then append new 'cards' dynamically
     return `
     <html lang="en">
         <head>
@@ -20,7 +20,9 @@ function generateHtml() { // returns a basic layout of the webpage, we can then 
             </div>
             
             <div class="container-fluid">
-                <div class="container-fluid row d-flex justify-content-center" id="manager"></div>
+                <div class="container-fluid row d-flex justify-content-center" id="Manager">
+                    ${managerStr}
+                </div>
             </div>
 
             <div class="container-fluid bg-primary text-center text-light mt-2">
@@ -28,7 +30,9 @@ function generateHtml() { // returns a basic layout of the webpage, we can then 
             </div>
 
             <div class="container-fluid">
-                <div class = "container-fluid row d-flex justify-content-center" id="engineer"></div>
+                <div class = "container-fluid row d-flex justify-content-center" id="Engineer">
+                    ${engineerStr}
+                </div>
             </div>
 
             <div class="container-fluid bg-primary text-center text-light mt-2">
@@ -36,7 +40,9 @@ function generateHtml() { // returns a basic layout of the webpage, we can then 
             </div>
 
             <div class="container-fluid">
-                <div class = "container-fluid row d-flex justify-content-center" id="intern"></div>
+                <div class = "container-fluid row d-flex justify-content-center" id="Intern">
+                    ${internStr}
+                </div>
             </div>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
         </body>
@@ -44,4 +50,41 @@ function generateHtml() { // returns a basic layout of the webpage, we can then 
     `;
 };
 
-module.exports = { generateHtml }
+function managerCard(data) {
+    return `
+                <div class="card ms-2" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">${data.getName() + " (ID: " + data.getId() + ")"}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${data.getRole() + " (Office: " + data.officeNumber + ")"}</h6>
+                        <a href="mailto: ${data.getEmail()}" class="card-link">Email</a>
+                    </div>
+                </div>
+    `;
+}
+
+function engineerCard(data) {
+    return `
+                <div class="card ms-2" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">${data.getName() + " (ID: " + data.getId() + ")"}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${data.getRole()}</h6>
+                        <a href="mailto: ${data.getEmail()}" class="card-link">Email</a>
+                        <a href="${data.getGithub()}" class="card-link">GitHub</a>
+                    </div>
+                </div>
+    `;
+}
+
+function internCard(data) {
+    return `
+                <div class="card ms-2" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">${data.getName() + " (ID: " + data.getId() + ")"}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${data.getRole() + " (" + data.getSchool() + ")"}</h6>
+                        <a href="mailto: ${data.getEmail()}" class="card-link">Email</a>
+                    </div>
+                </div>
+    `;
+}
+
+module.exports = { generateHtml, managerCard, engineerCard, internCard }
